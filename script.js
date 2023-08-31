dbb1a574dc405d4406a019a151bb5dff12f9cfd5
Narek Chakhoyan <narek.chakhoyan.g@gmail.com>
2023-08-31 12:11:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+//Annan 15 tarekan e isk Lusinen 7 tarov mec e Annaic
+//qani tarekan e Lusinen
+
+function ageOfLusine() {
+    let anna = 15;
+    return anna + 7;
+}
+
+console.log(ageOfLusine())
+
+//Qaxaqic gyux 100 km e , qani jamum khasni avtomeqenan ete nra aragutyuny 30km/j e ?
+
+function time(s, v) {
+    return s / v;
+}
+
+console.log(time(300, 30));
+
+
+//Aramy uni 5000 dram isk grichy arji 100dram
+//qani grich karox e gnel Aramy ?
+function hasPen(have, pens) {
+    return have / pens;
+}
+
+console.log(hasPen(7000, 100));
+
+//Hayrik = 35,txa = 5, hor ev txai tariqneri gumary havasar e papiki tariqi 1/2 in
+//qani tarekan e papiky?
+function ageOfgrandfather(dad, son) {
+    return (dad + son) * 2;
+}
+
+console.log(ageOfgrandfather(40, 6))
+
+
+//meqenan 480 km chanaparhy ancnum e 6 jamum
+//qani km/j e nra aragutyuny
+
+function theSpeedOfTheCar(way, time) {
+    return way / time;
+}
+
+console.log(theSpeedOfTheCar(700, 10));
+
